5036ca0be4610e05abcf5939a9b5ab703adcda27
Uraz Kağan GÜNEŞ <urazkagangunes@gmail.com>
2024-11-27 15:03:38 +0300

    Add project files.

---
+++ App.Repositories/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace App.Repositories;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ App.Repositories/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace App.Repositories;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = default!;
+    public decimal Price { get; set; }
+    public int Stock { get; set; }
+}
