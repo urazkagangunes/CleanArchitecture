3c70cf6891f25e250b1b41f8ca562a7358869fe6
Uraz Kağan GÜNEŞ <urazkagangunes@gmail.com>
2024-11-27 16:21:02 +0300

    generic repository added

--- App.Repositories/AppDbContext.cs
+++ App.Repositories/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using App.Repositories.Products;
+using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 
 namespace App.Repositories;
---
+++ App.Repositories/GenericRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace App.Repositories;
+
+public class GenericRepository<T>(AppDbContext appDbContext) : IGenericRepository<T> where T : class
+{
+    private readonly DbSet<T> _dbSet = appDbContext.Set<T>();
+
+    public async ValueTask AddAsync(T entity) => await _dbSet.AddAsync(entity);
+
+    public void Delete(T entity) => _dbSet.Remove(entity);
+
+    public IQueryable<T> GetAll() => _dbSet.AsQueryable().AsNoTracking();
+
+    public ValueTask<T?> GetByIdAsync(int id) => _dbSet.FindAsync(id);
+
+    public void Update(T entity) => _dbSet.Update(entity);
+
+    public IQueryable<T> Where(Expression<Func<T, bool>> predicate) => _dbSet.Where(predicate).AsNoTracking();
+
+}
