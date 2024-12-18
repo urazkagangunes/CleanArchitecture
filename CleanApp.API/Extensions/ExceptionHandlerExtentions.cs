3300c35d3e1509cc4a362cb2671598076e63cda3
Uraz Kağan GÜNEŞ <urazkagangunes@gmail.com>
2024-12-18 21:32:28 +0300

    Program.cs refactoring

---
+++ CleanApp.API/Extensions/ExceptionHandlerExtentions.cs
@@ -0,0 +1,15 @@
+﻿using CleanApp.API.ExceptionHandler;
+
+namespace CleanApp.API.Extensions
+{
+    public static class ExceptionHandlerExtentions
+    {
+        public static IServiceCollection AddExceptionHandlerExt(this IServiceCollection services)
+        {
+            services.AddScoped<CriticalExceptionHandler>();
+            services.AddScoped<GlobalExceptionHandler>();
+
+            return services;
+        }
+    }
+}
--- CleanApp.API/Program.cs
+++ CleanApp.API/Program.cs
@@ -1,47 +1,17 @@
-using App.Persistence.Extensions;
 using App.Application.Extensions;
-using CleanApp.API.Filters;
-using CleanApp.API.ExceptionHandler;
-using App.Application.Contracts.Caching;
-using App.Caching;
+using App.Persistence.Extensions;
+using CleanApp.API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
 
-builder.Services.AddControllers(options =>
-{
-    options.Filters.Add<FluentValidationFilter>();
-    options.SuppressImplicitRequiredAttributeForNonNullableReferenceTypes = true;
-});
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddControllerWithFiltersExt().AddSwaggerGenExt().AddExceptionHandlerExt().AddCachingExt();
 
 builder.Services.AddRepositories(builder.Configuration).AddServices(builder.Configuration);
 
-builder.Services.AddScoped(typeof(NotFoundFilter<,>));
-builder.Services.AddScoped<CriticalExceptionHandler>();
-builder.Services.AddScoped<GlobalExceptionHandler>();
-builder.Services.AddMemoryCache();
-builder.Services.AddSingleton<ICacheService, CacheService>();
-
 var app = builder.Build();
 
-app.UseExceptionHandler(x => { });
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+app.UseConfigurePipelineExt();
 
 app.MapControllers();
 
